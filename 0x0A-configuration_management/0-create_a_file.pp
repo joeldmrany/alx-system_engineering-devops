6018f2cbc995fde0a2677835244a8c48848b94ca
joeldmrany <joeldmrany1@gmail.com>
2024-06-28 22:52:43 +0300

    task: 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # creates a file in /tmp called school
 
-file { '/tmp/school' :
+file { '/tmp/school':
   ensure  => 'file',
   content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0774',
+  mode    => '0744',
 }
 
