14d6c75a08d3a9f982add5fec6545f6707a02c51
joeldmrany <joeldmrany1@gmail.com>
2024-08-13 13:09:40 +0300

    task: 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Task: 0
+"""
+
+
+def number_of_subscribers(subreddit):
+    """ that is how I get number of subscribers """
+    import requests
+
+    infos = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+
+    if infos.status_code >= 300:
+        return 0
+
+    return infos.json().get("data").get("subscribers")
