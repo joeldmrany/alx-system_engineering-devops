ea6de85ae069a93d4bf9d6fa4f13826a1b36cb95
joeldmrany <joeldmrany1@gmail.com>
2024-08-13 13:10:36 +0300

    Task: 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,6 @@ def number_of_subscribers(subreddit):
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-
     if infos.status_code >= 300:
         return 0
 
