b5ae2f035b09086e2ec903c1111f39963279eb6a
joeldmrany <joeldmrany1@gmail.com>
2024-08-15 01:31:15 +0300

    task: debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # it converts files with "phpp" into "php"
 
 exec{'fix-code':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path	=> '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
 
