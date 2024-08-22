3e25f74ac7378e804a7a1aeac694e08a4c18b9ba
joeldmrany <joeldmrany1@gmail.com>
2024-08-23 02:23:08 +0300

    user limitation

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# adjust user permisions
+
+# adjust hard limit
+exec { 'adjust-hard-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# adjust soft limit
+exec { 'adjust-soft-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
